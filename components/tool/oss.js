8738a334e21d3fd95ce7891c9a89ad378c7401e0
canwdev <c2097@live.com>
2019-12-03 18:23:52 +0800

    optimize

--- components/tool/oss.js
+++ components/tool/oss.js
@@ -8,17 +8,16 @@ module.exports = {
   async getOssPolicy(req, res) {
     try {
 
-      const dirPath = 'picture/test/' // 上传后例子：http://cqq.oss-cn-shenzhen.aliyuncs.com/testOSS/1489388301901，若为空，上传的文件则放到object的根目录
+      const dirPath = 'testOSS/' // 上传后例子：http://cqq.oss-cn-shenzhen.aliyuncs.com/testOSS/1489388301901，若为空，上传的文件则放到object的根目录
 
       const config = JSON.parse(fs.readFileSync(path.join(__dirname, 'oss_config.json'), {encoding: 'utf8'}))
-
-
 
       const OSSAccessKeyId = config.OSSAccessKeyId || '【这里填你阿里云的Access Key ID】'
       const secret = config.secret || '【这里填你阿里云的Access Key Secret】'
       const host = config.host || '【填你自己阿里云OSS的外网域名】' // http://cqq.oss-cn-shenzhen.aliyuncs.com
 
-      let end = new Date().getTime() + 300000
+      let now = new Date().getTime()
+      let end = now + 300000
       let expiration = new Date(end).toISOString()
       let policyString = {
         expiration,
@@ -33,11 +32,11 @@ module.exports = {
       res.json({
         code: CODE_OK,
         data: {
-          OSSAccessKeyId: OSSAccessKeyId,
+          OSSAccessKeyId,
           host,
           policy,
           signature,
-          saveName: end,
+          saveName: now, // 此项不是必要的
           startsWith: dirPath
         }
       })
